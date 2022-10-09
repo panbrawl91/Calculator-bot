d98cfa73316a856c3e89d7a5efd9c41e9893fc75
PanGames <109430498+panbrawl91@users.noreply.github.com>
2022-10-09 19:53:04 +0300

    Add files via upload

---
+++ CalcBot.py
@@ -0,0 +1,25 @@
+print("Hello, user")
+xlx = 100
+pan91 = 1000
+print(f" Write an answer - {xlx} + {pan91}")
+Answer = int(input("Answer:"))
+print(f" Your Answer - {Answer == xlx + pan91}")
+print("Correct Answer is: 1100")
+
+
+
+olax_1 = 11
+olax_2 = 1709
+print(f" Write an answer - {olax_1} + {olax_2}")
+Answer = int(input("Answer:"))
+print(f" Your Answer - {Answer == olax_1 + olax_2}")
+
+print("Correct Answer is: 1720")
+
+
+
+
+print("Good job!")
+print("Program by PanBrawl91")
+
+# Hello, user
